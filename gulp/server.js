faccba729046cc69a8bb99cd52108a2d324a7070
Brad Westfall <brad@azpixels.com>
2016-06-16 10:59:01 -0700

    Update 2016-06-16T17:58:58.442Z

--- gulp/server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var serve = require('gulp-serve');
-
-gulp.task('server', serve({
-    root: ['./public'],
-    port: 8080
-}));
--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var sass = require('./gulp/sass');
-var server = require('./gulp/server');
-var deploy = require('./gulp/deploy');
-
-gulp.task('default', ['sass', 'watch:sass', 'server']);
