05d290b1cd14edc886aaa47f3884ea72b0121e11
Brad Westfall <brad@azpixels.com>
2016-06-16 10:58:44 -0700

    deploy code

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('./gulp/sass')
-var server = require('./gulp/server')
+var sass = require('./gulp/sass');
+var server = require('./gulp/server');
+var deploy = require('./gulp/deploy');
 
 gulp.task('default', ['sass', 'watch:sass', 'server']);
