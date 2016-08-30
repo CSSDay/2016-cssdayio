d6fcf5b4b6119e1456b9d17e7d46f38dd281cf72
Brad Westfall <brad@azpixels.com>
2016-08-30 13:49:54 -0700

    speakers

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,34 @@
 $(function() {
 
-    $(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
+  $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
 
-        if (scroll >= 20) {
-            $('.page').addClass('scroll');
-        } else {
-            $('.page').removeClass('scroll');
-        }
-    });
+    if (scroll >= 20) {
+      $('.page').addClass('scroll');
+    } else {
+      $('.page').removeClass('scroll');
+    }
+  });
 
-    $('.menu-on').on('click', function() {
-        $('.page').addClass('show-menu');
-    })
+  $('.menu-on').on('click', function() {
+    $('.page').addClass('show-menu');
+  });
 
-    $('.menu-off-tint, .menu-off-button, .primary-nav nav').on('click', function() {
-        $('.page').removeClass('show-menu');
-    })
+  $('.menu-off-tint, .menu-off-button, .primary-nav nav').on('click', function() {
+    $('.page').removeClass('show-menu');
+  });
+
+  $('.name, .image-wrap', '.speaker-list .speaker:not(.no-click)').on('click', function(e) {
+    e.preventDefault();
+    $('.page').addClass('show-profile');
+    var speaker = $(this).data('speaker');
+    $('.speaker.' + speaker).show();
+  });
+
+
+  $('.speaker-profiles-tint').on('click', function() {
+    $('.page').removeClass('show-profile');
+    $('.speaker-profiles .speaker').hide();
+  });
 
 })
