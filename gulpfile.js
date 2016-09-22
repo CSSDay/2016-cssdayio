6a8f7e363b7a0f132e1e87339d78743ff40957fb
Brad Westfall <brad@azpixels.com>
2016-09-22 12:21:54 -0700

    remove svg sprite

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,5 @@ var gulp = require('gulp');
 var sass = require('./gulp/sass');
 var server = require('./gulp/server');
 var deploy = require('./gulp/deploy');
-var svgSprite = require('./gulp/svg-sprite');
 
-gulp.task('default', ['sass', 'svg-sprite', 'watch:sass', 'watch:svg-sprite', 'server']);
+gulp.task('default', ['sass', 'watch:sass', 'server']);
